7b7aedd14ff8aba9c1a5bd6db9828293a374bcbb
Over The Top <overthetop_for_ever@live.fr>
2021-01-29 00:22:14 +0100

    Add and Modification

--- ISMDecrypt/Form1.vb
+++ ISMDecrypt/Form1.vb
@@ -6,7 +6,28 @@ Public Class Form1
         Dim ResultExtChrome = MsgBox("Widevine Chrome Extension isn't installed ?", vbApplicationModal + vbYesNo + vbExclamation, "Widevine Chrome Extension")
         ' si ce n'est pas installé, on va ouvrir le dossier a dezipper et un fichier texte avec le bloc-note pour donner les infos 
         If MsgBoxResult.No Then
-            ZipFile.ExtractToDirectory(WidevineL3Zip, $"{PathDocument}\Widevine\", True)
+            Try
+                MsgBox("Widevine is installed in Documents Folder", vbApplicationModal + vbOKOnly + vbInformation, "Information")
+                If IO.Directory.Exists($"{PathDocument}\Widevine\") = False Then
+                    ZipFile.ExtractToDirectory(WidevineL3Zip, $"{PathDocument}\Widevine\", False)
+                End If
+                ' Process.Start("explorer.exe", $"{PathDocument}\Widevine\")
+                Process.Start(ChromeApp, PageExtension)
+
+            Catch ex As Exception
+                MsgBox($"Error: {ex.Message}{vbCrLf} Folder Addon or files not found.", vbApplicationModal + vbExclamation + vbOKOnly, $"Error {ex.HResult}")
+                ' L'erreur est présente mais on continue quand même 
+            End Try
+        End If
+        'On doit aussi décompresser le fichier Bento.zip dans le dossier de l'application
+        If IO.File.Exists(DecryptApp) = False Then
+            Try
+                MsgBox("Bento is installed in Util Folder", vbApplicationModal + vbOKOnly + vbInformation, "Information")
+                ZipFile.ExtractToDirectory(BentoZip, $"{PathUtil}", False)
+
+            Catch ex As Exception
+                MsgBox($"Error: {ex.Message}{vbCrLf} Folder Addon or files not found.", vbApplicationModal + vbExclamation + vbOKOnly, $"Error {ex.HResult}")
+            End Try
         End If
     End Sub
 End Class
--- ISMDecrypt/Variable.vb
+++ ISMDecrypt/Variable.vb
@@ -1,5 +1,9 @@
 ﻿Module Variable
     Public WidevineL3Zip As String = $"{Application.StartupPath}\Addon\widevineL3Decryptor_Chrome_Extension.zip"
+    Public DecryptApp As String = $"{Application.StartupPath}\Util\bin\mp4decrypt.exe"
     Public BentoZip As String = $"{Application.StartupPath}\Addon\Bento4.zip"
     Public PathDocument As String = $"{My.Computer.FileSystem.SpecialDirectories.MyDocuments}"
+    Public PathUtil As String = $"{Application.StartupPath}\Util\"
+    Public ChromeApp As String = $"{My.Computer.FileSystem.SpecialDirectories.ProgramFiles}\Google\Chrome\Application\chrome.exe"
+    Public PageExtension As String = $"chrome{Chr(58)}//extensions"
 End Module
